65776616c0e6bbfee3f91966e93f6ec93d6e787c
Jeron Osguthorpe <jeronosg@outlook.com>
2023-04-21 18:10:54 -0600

    Update Interface.java
    Updated the User Interface. This allows the user to use inputs to create worlds, advance a day, leave the world, create a new one, or quit the simulation altogether.

--- src/Interface.java
+++ src/Interface.java
@@ -7,6 +7,7 @@ public class Interface {
         System.out.println("You can run the followng commands:");
         System.out.println("N - New World");
         System.out.println("Q - Quit Simulation");
+        System.out.println("LIST - List of Commands");
         System.out.println("");
 
         String quitSimulation = "";
@@ -15,8 +16,11 @@ public class Interface {
             switch (quitSimulation) {
                 case "Q":
                     System.out.println("Quitting Simulation");
+                    quitSimulation = "Q";
                     break;
+
                 case "N":
+                    Scanner inGame = new Scanner(System.in);
                     System.out.println("New World Created:");
                     System.out.println("Type D to go to Next Day, Press L to Leave");
                     World world = new World();
@@ -41,19 +45,46 @@ public class Interface {
                     }
                     String leaveWorld = "";
                     while(leaveWorld != "L"){
-                        if (userInput.next() == "L"){
-                            System.out.println("Leaving World");
-                            System.out.println("To Start a New World, Press N.");
-                            leaveWorld = "L";
-                            break;
-                        } else if (userInput.next() == "N"){
-                            //TODO: Add Day
-                            System.out.println("Next Day");
-                            break;
-                        } else {
-                            System.out.println("Invalid Input");
+                        leaveWorld = inGame.next();
+                        switch (leaveWorld) {
+                            case "L":
+                                System.out.println("Leaving World");
+                                System.out.println("Type N to create a new world, Press Q to Quit Simulation");
+                                System.out.println("");
+                                leaveWorld = "L";
+                                break;
+
+                            case "D":
+                                System.out.println("Next Day");
+                                System.out.println("");
+                                //TODO: Add code to go to next day
+                                break;
+
+                            case "LIST":
+                                System.out.println("D - Next Day");
+                                System.out.println("L - Leave World");
+                                System.out.println("LIST - List of Commands");
+                                System.out.println("");
+                                break;
+        
+                            default:
+                                System.out.println("Invalid Command");
+                                System.out.println("");
+                                break;
                         }
                     }
+
+                case "LIST":
+                    System.out.println("N - New World");
+                    System.out.println("Q - Quit Simulation");
+                    System.out.println("LIST - List of Commands");
+                    System.out.println("");
+                    break;
+                
+                default:
+                    System.out.println("Invalid Command");
+                    System.out.println("");
+                    break;
                 }
         }
         userInput.close();
