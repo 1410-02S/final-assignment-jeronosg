3395955de9a6481c0edcebbad0f43f5770c388c2
Jeron Osguthorpe <jeronosg@outlook.com>
2023-04-21 18:58:43 -0600

    Day Function Added...ish
    I added the day function, and as of right now, it's working great..ish. I need to make it so that the creature(s) stay the same after each day, but I don't quite know how to do that.

--- src/Interface.java
+++ src/Interface.java
@@ -1,6 +1,18 @@
-import java.util.Scanner;   
+/*
+ * Final Assignment - CS 1010.
+ * @author jeronosg
+ * @version CS 1410
+ * 
+ */
+
+// Import Statements
+import java.util.Scanner; 
+
+// Start of Progam
 public class Interface {
+    // Main Method
     public static void main(String[] args) throws Exception {
+        // Start of User Interface
         Scanner userInput = new Scanner(System.in);
         System.out.println("Hello to the World of Creatures!");
         System.out.println("Running the program will create a random world that will have a random amount of creatures, food, and a biome.");
@@ -14,11 +26,13 @@ public class Interface {
         while(quitSimulation != "Q"){
             quitSimulation = userInput.next();
             switch (quitSimulation) {
+                // Quit Simulation
                 case "Q":
                     System.out.println("Quitting Simulation");
                     quitSimulation = "Q";
                     break;
-
+                
+                // New World
                 case "N":
                     Scanner inGame = new Scanner(System.in);
                     System.out.println("New World Created:");
@@ -26,27 +40,19 @@ public class Interface {
                     World world = new World();
                     int amountOfCreatures = world.randomCreature();
                     int amountOfFoodSpawned = world.randomFoodSpawn();
-                    String worldBiome = world.biome();
                     System.out.println("Amount of Creatures: " + amountOfCreatures);
                     System.out.println("Food Spawned: " + amountOfFoodSpawned);
-                    System.out.println("Biome: " + worldBiome);
+                    System.out.println("Biome Modifier: " + world.biomeModifier);
+                    System.out.println("Biome: " + world.biome);
                     System.out.println("");
                     
-                    for (int i = 1; i < amountOfCreatures + 1; i++){
-                        Creature creature = new Creature();
-                        System.out.println("Creature Number: " + i);
-                        System.out.print("Creature Type: " + creature.creatureType());
-                        System.out.print("  Reproduction Rate: " + creature.reproductionRate());
-                        System.out.print("  Chance of Death: " + creature.chanceOfDeath());
-                        System.out.print("  Amount Eaten: " + creature.eat(0));
-                        System.out.print("  Alive: " + creature.status());
-                        System.out.println(" ");
-                        System.out.println(" ");
-                    }
+                    world.printData();
+
                     String leaveWorld = "";
                     while(leaveWorld != "L"){
                         leaveWorld = inGame.next();
                         switch (leaveWorld) {
+                            // Leave World
                             case "L":
                                 System.out.println("Leaving World");
                                 System.out.println("Type N to create a new world, Press Q to Quit Simulation");
@@ -54,33 +60,39 @@ public class Interface {
                                 leaveWorld = "L";
                                 break;
 
+                            // Next Day
                             case "D":
                                 System.out.println("Next Day");
                                 System.out.println("");
-                                //TODO: Add code to go to next day
+                                world.printData();
+
                                 break;
 
+                            // List of Commands
                             case "LIST":
                                 System.out.println("D - Next Day");
                                 System.out.println("L - Leave World");
                                 System.out.println("LIST - List of Commands");
                                 System.out.println("");
                                 break;
-        
+    
+                            // Invalid Command
                             default:
                                 System.out.println("Invalid Command");
                                 System.out.println("");
                                 break;
                         }
                     }
-
+                
+                // List of Commands
                 case "LIST":
                     System.out.println("N - New World");
                     System.out.println("Q - Quit Simulation");
                     System.out.println("LIST - List of Commands");
                     System.out.println("");
                     break;
-                
+
+                // Invalid Command
                 default:
                     System.out.println("Invalid Command");
                     System.out.println("");
--- src/World.java
+++ src/World.java
@@ -10,29 +10,48 @@ import java.util.Random;
 
 // Stat of Progam
 public class World {
+    // Variables 
+    String biome = "";
+    int biomeModifier = 0;
+    int amountOfCreatures = 0;
+    int gameDay = 0;
+
+    // Amount of Creatures Spawned
     public int randomCreature(){
         Random rand = new Random();
         int randomCreatureSpawn = rand.nextInt(5) + 1;
+        amountOfCreatures = randomCreatureSpawn;
         return randomCreatureSpawn;
     }
 
+    // Amount of Food Spawned Based on Biome
     public int randomFoodSpawn(){
         Random rand = new Random();
         int randomFoodSpawn = rand.nextInt(10) + 1;
         if (biome() == "Desert") {
-            return randomFoodSpawn - 2;
-            // If the biome is a desert, there will be 1 less food spawn.
+            biomeModifier = -2;
+            return randomFoodSpawn + biomeModifier;
+            // If the biome is a desert, there will be 2 less food spawn.
         } else if (biome() == "Forest"){
-            return randomFoodSpawn + 2;
-            // If the biome is a forest, there will be 1 more food spawn.
+            biomeModifier = 2;
+            return randomFoodSpawn + biomeModifier;
+            // If the biome is a forest, there will be 2 more food spawn.
         } else if (biome() == "Tundra"){
-            return randomFoodSpawn - 1;
+            biomeModifier = -1;
+            return randomFoodSpawn + biomeModifier;
+            // If the biome is a tundra, there will be 1 less food spawn.
         } else if (biome() == "Jungle"){
-            return randomFoodSpawn + 1;
+            biomeModifier = 1;
+            return randomFoodSpawn + biomeModifier;
+            // If the biome is a jungle, there will be 1 more food spawn.
         } else if (biome() == "Ocean"){
-            return randomFoodSpawn;
+            biomeModifier = 0;
+            return randomFoodSpawn + biomeModifier;
+            // If the biome is a ocean, there will be no change in food spawn.
         } else {
+            biomeModifier = 0;
             return randomFoodSpawn;
+            // Fail Safe. Defualt: 0
         }
     }
 
@@ -40,17 +59,38 @@ public class World {
         Random rand = new Random();
         int biomeType = rand.nextInt(5) + 1;
         if (biomeType == 1){
+            biome = "Forest";
             return "Forest";
         } else if (biomeType == 2){
+            biome = "Desert";
             return "Desert";
         } else if (biomeType == 3){
+            biome = "Tundra";
             return "Tundra";
         } else if (biomeType == 4){
+            biome = "Jungle";
             return "Jungle";
         } else if (biomeType == 5){
+            biome = "Ocean";
             return "Ocean";
         } else {
             return "Forest";
         }
+    }
+
+    public void printData() {
+        System.out.println("Day: " + gameDay);
+        for (int i = 1; i < amountOfCreatures + 1; i++){
+            Creature creature = new Creature();
+            System.out.println("Creature Number: " + i);
+            System.out.print("Creature Type: " + creature.creatureType());
+            System.out.print("  Reproduction Rate: " + creature.reproductionRate());
+            System.out.print("  Chance of Death: " + creature.chanceOfDeath());
+            System.out.print("  Amount Eaten: " + creature.eat(0));
+            System.out.print("  Alive: " + creature.status());
+            System.out.println(" ");
+            System.out.println(" ");
+        }
+        gameDay = gameDay + 1;
     }
 }
